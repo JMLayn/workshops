c39b48fae340da982f22af060b72eb3db4ec1f59
root <root@chefclient.jmlayn.com>
2017-11-11 23:47:48 -0500

    tomcat yum repo end of Sat

--- 2_Tomcat/install_tomcat.rb
+++ 2_Tomcat/install_tomcat.rb
@@ -33,3 +33,27 @@ user 'tomcat' do
 	group 'tomcat'	   # puts in tomcat group; same as -g
 	shell '/bin/nologin' # sets shell to no login; same as -s
 end
+
+#Download the Tomcat Binary
+#$ cd /tmp
+#$ wget http://apache.cs.utah.edu/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz
+
+#used remote_file resource to grab the file. Set action create if missing so will download when
+#missing 
+remote_file '/tmp/apache-tomcat-8.5.23.tar.gz' do
+	source 'https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.23/bin/apache-tomcat-8.5.23.tar.gz'
+	action :create_if_missing
+end
+
+#Extract the Tomcat Binary
+#$ sudo mkdir /opt/tomcat
+
+directory '/opt/tomcat' 
+
+#$ sudo tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1
+
+execute 'extract java tarball' do
+	command 'tar xvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
+	cwd '/tmp'
+	not_if { File.exists?("/opt/tomcat/lib/catalina.jar")}
+end
